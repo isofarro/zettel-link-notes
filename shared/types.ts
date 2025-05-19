a89a9fffea10477ab66a57c2bf5cc3e42e8cea0a
MikeD <isofarro2@gmail.com>
2025-05-19 20:12:17 +0100

    Add shared types generated by the backend.

---
+++ shared/types.ts
@@ -0,0 +1,48 @@
+export interface Note {
+  id: number;
+  note_id: string;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+  current_version_id: number;
+}
+
+export interface NoteVersion {
+  id: number;
+  note_id: number;
+  title: string;
+  content: string;
+  created_at: string;
+}
+
+export interface NoteMetadata {
+  id: number;
+  note_id: number;
+  key: string;
+  value: string;
+}
+
+export interface NoteTerm {
+  note_id: number;
+  term_id: number;
+}
+
+export interface NoteReference {
+  parent_note_id: number;
+  child_note_id: number;
+}
+
+export interface Taxonomy {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface TaxonomyTerm {
+  id: number;
+  taxonomy_id: number;
+  name: string;
+  description: string;
+}
